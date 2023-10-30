a8246957b77a045e884c1ae29f4ef6740bc22274
mahabube <info.freelancermahabub@gmail.com>
2023-10-30 14:08:38 +0600

    added done

---
+++ DataType.py
@@ -0,0 +1,18 @@
+# Sting Data Type
+name ="mahabub"
+print(name)
+
+# init type Data
+mahabub = 240
+print(mahabub)
+print(type(mahabub))
+
+# Floating Type Data
+
+mahabub1  = 210.2
+print(type(mahabub1))
+
+#Complex type Data
+
+mahabub3  = 4200j
+print(type(mahabub3))
